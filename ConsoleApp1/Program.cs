ce5e381fc9f4a532ad5d021038d3b4e2d1266d83
vegan13 <vegan13@o2.pl>
2023-11-26 11:49:44 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lucjan";
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Nazywam się Lucjan i tworzę moją aplikację, która uwolni mnie od etatu i da wolność finansową");
+        }
+    }
+
+
+
+}
